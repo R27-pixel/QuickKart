9b530948c985ac349af78aeb01015bc739af9d5c
arya-912 <pravesharya2004@gmail.com>
2025-05-19 16:08:45 +0530

    Initial client-side

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,25 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
-import HeroSection from "./components/HeroSection";
-import FeatureSection from "./components/FeatureSection";
-import ProductBenefitsSection from "./components/ProductBenefitsSection";
-import ExperienceSection from "./components/ExperienceSection";
-import CallToActionSection from "./components/CallToActionSection";
-import Footer from "./components/Footer";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage"
+import AdminLogin from './pages/admin/AdminLogin';
+
+// import Header from "./components/Header";
+// import HeroSection from "./components/HeroSection";
+// import FeatureSection from "./components/FeatureSection";
+// import ProductBenefitsSection from "./components/ProductBenefitsSection";
+// import ExperienceSection from "./components/ExperienceSection";
+// import CallToActionSection from "./components/CallToActionSection";
+// import Footer from "./components/Footer";
 
 export default function App() {
   return (
     <div className="font-sans text-gray-800">
-      <Header />
-      <HeroSection />
-      <FeatureSection />
-      <ProductBenefitsSection />
-      <ExperienceSection />
-      <CallToActionSection />
-      <Footer />
+      
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/admin-login' element={<AdminLogin />} />
+        </Routes>
+      
     </div>
   );
 }
---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "../components/Header";
+import HeroSection from "../components/HeroSection";
+import FeatureSection from "../components/FeatureSection";
+import ProductBenefitsSection from "../components/ProductBenefitsSection";
+import ExperienceSection from "../components/ExperienceSection";
+import CallToActionSection from "../components/CallToActionSection";
+import Footer from "../components/Footer";
+
+export default function HomePage() {
+  return (
+    <div className="font-sans text-gray-800">
+      <Header />
+      <HeroSection />
+      <FeatureSection />
+      <ProductBenefitsSection />
+      <ExperienceSection />
+      <CallToActionSection />
+      <Footer />
+    </div>
+  );
+}
+
